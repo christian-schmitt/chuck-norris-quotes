1cec4311ff479fede9a0883d2859e8b1378158c3
Christian Schmitt <schmitt.christian@gmx.de>
2020-10-14 12:26:01 +0200

    Some Configuration and Properties test

--- src/main/java/guru/springframework/chucknorrisquotes/JokeApplication.java
+++ src/main/java/guru/springframework/chucknorrisquotes/JokeApplication.java
@@ -1,12 +1,25 @@
 package guru.springframework.chucknorrisquotes;
 
+import guru.springframework.chucknorrisquotes.config.ConfigProperties;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
-@ImportResource("classpath:chuck-config.xml")
+//@ImportResource("classpath:chuck-config.xml")
+@ImportResource(locations = {"classpath:chuck-config.xml"})
 public class JokeApplication {
+
+    final ConfigProperties configProperties;
+
+    public JokeApplication(ConfigProperties configProperties) {
+        this.configProperties = configProperties;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JokeApplication.class, args);
--- src/test/java/guru/springframework/chucknorrisquotes/JokeApplicationTests.java
+++ src/test/java/guru/springframework/chucknorrisquotes/JokeApplicationTests.java
@@ -1,13 +1,22 @@
 package guru.springframework.chucknorrisquotes;
 
+import guru.springframework.chucknorrisquotes.config.ConfigProperties;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JokeApplicationTests {
 
+    @Autowired
+    ConfigProperties configProperties;
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void testStuff() {
+        Assert.assertNotNull(configProperties);
+    }
 }
